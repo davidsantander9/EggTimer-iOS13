396253db170e0d09168eaab844ab8846afbf179e
davidsantander9@gmail.com <davidsantander9@gmail.com>
2023-10-31 22:44:32 -0600

    add timmer functionallity

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    let eggCookingMinutes: [String: Int] = [
+        "Soft": 5,
+        "Medium": 7,
+        "Hard": 12,
+    ]
+    
+    let seconds: Int = 60
+    
+    var countdownTimer: Timer?
+    var secondsRemaining = 0
+    var timePassed = 0
+    
+    @IBAction func cookEggAction(_ sender: UIButton) {
+        let hardness = sender.currentTitle
+        
+        secondsRemaining = seconds * eggCookingMinutes[hardness ?? "Soft"]!
+        
+        timePassed = 0
+        
+        countdownTimer?.invalidate()
+        
+        startTimer()
+        
+        print(getFormatTimer(seconds: secondsRemaining))
+    }
+    
+    func startTimer(){
+        countdownTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in self?.updateTimer()}
+    }
+    
+    @objc func updateTimer() {
 
+           // Increment the time passed
 
+           timePassed += 1
+           if timePassed < secondsRemaining {
+               timerLabel.text = getFormatTimer(seconds: secondsRemaining - timePassed)
+           } else {
+               countdownTimer?.invalidate()
+               timerLabel.text = "Done"
+           }
+       }
+    
+    func getFormatTimer(seconds: Int) -> String{
+        let formatter = DateComponentsFormatter()
+        formatter.allowedUnits = [.minute, .second]
+        formatter.zeroFormattingBehavior = .dropLeading
+        formatter.unitsStyle = .short
+        
+        let secondsTimeInterval: TimeInterval = TimeInterval(seconds)
+        
+        return formatter.string(from: secondsTimeInterval) ?? "0"
+    }
+    
 }
